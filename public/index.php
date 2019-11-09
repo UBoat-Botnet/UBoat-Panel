d88c2a25e1d9a52e75dfdf09b3c129204054962f
Jorge Matricali <jorgematricali@gmail.com>
2019-11-09 19:27:20 -0300

    Fixed WEB_DIR

--- public/index.php
+++ public/index.php
@@ -7,8 +7,8 @@ session_start();
 // Defines
 define('ROOT_DIR', realpath(dirname(__FILE__) . '/../'));
 define('APP_DIR', ROOT_DIR .'/src');
-define('WEB_BASE', sprintf("%s",pathinfo($_SERVER['PHP_SELF'])['dirname']));
-define('WEB_DIR' , WEB_BASE . '/www/');
+define('WEB_BASE', realpath(__DIR__));
+define('WEB_DIR' , WEB_BASE);
 
 spl_autoload_register(function ($class) {
     include APP_DIR . '/' . $class . '.php';
